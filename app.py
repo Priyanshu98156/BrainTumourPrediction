9a366476469038d84f8e519cfe111bdcc62f7054
Priyanshu98156 <109512538+Priyanshu98156@users.noreply.github.com>
2025-04-02 13:29:47 +0530

    update

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import tensorflow as tf
 from PIL import Image
 import numpy as np
-
+import cv2
 #load model
 def load_model():
     try:
@@ -12,13 +12,28 @@ def load_model():
         st.error(f"Error loading model: {e}")
         return None
     
-    
-def preprocessing_image(image):
-    image = image.resize((128,128))
-    image = np.array(image)/255.0
-    image = np.expand_dims(image,axis = 0 )    
-    
-    return image
+def preprocessing_image(uploaded_file):
+    # Convert PIL Image to NumPy array
+    image = np.array(uploaded_file)  
+
+   #ensure 3 channels (RGB) before converting to grayscale
+    if len(image.shape) == 2:
+        gray_image = image
+    else:
+        gray_image = cv2.cvtColor(image,cv2.COLOR_RGB2GRAY)
+
+    gray_image = cv2.resize(gray_image,(128,128))
+
+    gray_image = gray_image/255.0
+
+    #Expand dimensions for model input (1,128,128,-1)
+    gray_image = np.expand_dims(gray_image,axis=(0,-1))
+
+    return gray_image
+
+
+
+
 model = load_model()
 
 
@@ -31,10 +46,12 @@ if uploaded_file:
     processed_image = preprocessing_image(image)
     prediction = model.predict(processed_image)[0][0]
 
+    if model:  # Ensure model is loaded
+        prediction = model.predict(processed_image)[0][0]
 
-    if prediction >  0.5:
-        st.error("*** Tumor Detected***")
-    else:
-        st.success("**No Tumor Detected")
+        if prediction > 0.5:
+            st.error("*** Tumor Detected ***")
+        else:
+            st.success("**No Tumor Detected**")
 
-    st.write(f"Confidence :{prediction:.2f}")
+        st.write(f"Confidence: {prediction:.2f}")
